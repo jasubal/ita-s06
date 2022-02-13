f080fdae216198dbbe63cf54d12c707e754d6b24
Jaume Subirats <jaume.subirats@gmail.com>
2022-02-13 22:48:17 +0100

    finished Ex6b

--- components/escena.js
+++ components/escena.js
@@ -1,39 +1,29 @@
 Vue.component('escena', {
 
- template:
-    `
+ template:`
     <div class="comp c-escena">
     <ul class="lesfrases">
-    <li v-for="frase of frases">
-            {{frase.txt}}
-    </li>
+<li class="frase" v-for="(item, i ) in frases" :key="i" :class="{ active: i === currentSentence-1}" >
+{{item.txt}}
+</li>
   </ul>
     </div>
-    `
-,
-// <button @click="numero++">+</button>-->
-props: ['numeroFill'],
+    `,
 
-
+props: ['frases','currentSentence'],
 data(){
     return{
         nom: "escena",
-        frases: [
-            {txt:'El nostre heroi estava surant per l´espai sideral quan a la llunyania va albirar una nau espacial '},
-            {txt:'Sentia curiositat per l´interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes. '},
-            {txt:'L´heroi va decidir travessar la porta que el portava a casa '},
-            {txt:'Mentrestant altres heroes no van tenir tanta sort en la seva elecció '}
-          ],
-        }
+      }
+},
+  methods: {
+/*
+    selectItem(i) {
+      this.activeItem = i;
     },
-    methods:{
-           numfrases(){
-            this.frases.length;
+    */
 
-           }
-    }
-
-
+  }
 
 
 })
